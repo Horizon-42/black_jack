b7c369cf533c1551346e3287a1dcb13121a3e141
horizon <liudxp@live.cn>
2025-05-21 22:48:22 +0200

    Initialize BlackJackGame class with player and dealer hand setup

--- models/blackjack_game.py
+++ models/blackjack_game.py
@@ -2,3 +2,23 @@ from player import Player
 from card import Card
 from hand import Hand
 from deck import Deck
+from dealer import Dealer
+
+
+class BlackJackGame(object):
+    players: list[Player]
+    dealer: Dealer
+    deck: Deck
+
+    def __init__(self, player_num: int = 1):
+        self.deck = Deck()
+        self.deck.shuffle()
+        # to do or not?
+        self.deck.burn_out()
+
+        # init hand for players
+        for _ in range(player_num):
+            self.players.append(
+                Player(self.deck.deal_card(), self.deck.deal_card()))
+        # init hand for dealer
+        self.dealer = Dealer(self.deck.deal_card(), self.deck.deal_card())
